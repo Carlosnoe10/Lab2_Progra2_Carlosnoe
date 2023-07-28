a8e97988ca91632db9a7aae8a6409453becd3544
Carlosnoe10 <carlosdnoem@gmail.com>
2023-07-28 13:19:51 -0700

    Termine el login ahora voy a hacer el primer menu .-.    Hasta ahorita el login lo hice recursivo porque? porque si. aun sin validaciones, odio las validaciones pero ni modo

--- Lab2Progra2_CarlosNoé/src/lab2progra2_carlosnoé/Lab2Progra2_CarlosNoé.java
+++ Lab2Progra2_CarlosNoé/src/lab2progra2_carlosnoé/Lab2Progra2_CarlosNoé.java
@@ -1,23 +1,52 @@
 package lab2progra2_carlosnoé;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab2Progra2_CarlosNoé {
 
     public static void main(String[] args) {
-        boolean Log=true;
-        while (Log=true) {            
+        ArrayList<Gerente> ING = new ArrayList();
+        ING = PreLogin(ING);
+        boolean Log = true;
+        while (Log == true) {
             
-        
         }
     }
-    public static boolean Login(boolean Key){
+
+    public static ArrayList<Gerente> PreLogin(ArrayList<Gerente> ING) {
+        String one = "gerente";
+        String contra = "g3r$nt0";
+
+        Gerente Gerr = new Gerente(one, contra);
+        ING.add(Gerr);
+        return ING;
+    }
+
+    public static boolean Login(boolean Key, ArrayList<Gerente> ING) {
         Scanner k1ng = new Scanner(System.in);
-        System.out.println("Bienvenido al Login de Gerentes");
+        Scanner Queen = new Scanner(System.in);
+        System.out.println("--Bienvenido al Login de Gerentes--");
         System.out.println("Ingrese su nombre de usuario");
+        String Nombre = k1ng.nextLine();
+        System.out.println("Ingrese la Contrasenya");
+        String Contrasenya = Queen.nextLine();
+        Scanner k1NT = new Scanner(System.in);
+        System.out.println("Desea Salir? Escriba 1");
+        int Num = k1ng.nextInt();
+        Gerente Gerr = new Gerente(ING.get(0).getUsuario(), ING.get(0).getContra());
+
+        if ((ING.get(0).getUsuario().equalsIgnoreCase(Nombre)) && (ING.get(0).getContra().equalsIgnoreCase(Contrasenya)) && ((Num != 1))) {
+            return Key;
+
+        } else if ((ING.get(0).getUsuario().equalsIgnoreCase(Nombre)) && (ING.get(0).getContra().equalsIgnoreCase(Contrasenya)) && ((Num == 1))) {
+            return false;
+        } else {
+            System.out.println("Usuario o Contrasenya Equivocados");
+            return Login(Key, ING);
+        }
         
-    }    
-        
-    
-    
+
+    }
+
 }
